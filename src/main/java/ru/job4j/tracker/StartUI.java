77e6fb496943e3fdc4f32d1ce55e0b50de8c213a
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-02 21:37:04 +0700

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,8 +3,6 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void main(String[] args) {
         Item newItem = new Item();
-        Tracker tracker = new Tracker();
-        tracker.add(newItem);
-        System.out.println(tracker.findById(newItem.getId()));
+        System.out.println(newItem);
     }
 }
