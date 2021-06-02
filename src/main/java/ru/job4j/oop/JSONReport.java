dbba25d3679019444a112500e9727987b0d68199
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-02 16:09:06 +0700

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.oop;
 
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + "\"" + name + "\"" + " : " + "\"" + name + "\"," + System.lineSeparator()
