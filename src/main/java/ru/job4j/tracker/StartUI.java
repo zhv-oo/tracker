d49af3fbc770b05ddfea893af07c76f8a76245be
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-03 22:41:41 +0700

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,6 +16,7 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
+                System.out.println("Item added: " + item);
             } else if (select == 1) {
                 Item[] items = tracker.findAll();
                 if (items.length != 0) {
