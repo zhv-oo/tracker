c37587c9c4f1b825b90b70f622b9345d0c235b06
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-07 21:10:20 +0700

    2. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,81 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class SingleTracker {
+    private static SingleTracker singleTracker = null;
+
+    private final Item[] items = new Item[100];
+
+    private int ids = 1;
+    private int size = 0;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getSingleTracker() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
+        }
+        return singleTracker;
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int i = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl[i++] = item;
+            }
+        }
+        return Arrays.copyOf(rsl, i);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, size);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
