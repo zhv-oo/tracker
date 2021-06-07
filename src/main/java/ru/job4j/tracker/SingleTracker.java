d41bd7201f8dc5f62deed629808362020fcda62f
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-07 21:17:55 +0700

    2. Singleton [#94606 #165637]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,14 +1,9 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
-
 public class SingleTracker {
     private static SingleTracker singleTracker = null;
 
-    private final Item[] items = new Item[100];
-
-    private int ids = 1;
-    private int size = 0;
+    private Tracker tracker = new Tracker();
 
     private SingleTracker() {
     }
@@ -21,61 +16,26 @@ public class SingleTracker {
     }
 
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
+        return tracker.add(item);
     }
 
     public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return tracker.findById(id);
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[size];
-        int i = 0;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getName().equals(key)) {
-                rsl[i++] = item;
-            }
-        }
-        return Arrays.copyOf(rsl, i);
+        return tracker.findByName(key);
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(this.items, size);
+        return tracker.findAll();
     }
 
     public boolean replace(int id, Item item) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            item.setId(id);
-            items[index] = item;
-        }
-        return rsl;
+        return tracker.replace(id, item);
     }
 
     public boolean delete(int id) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
-        }
-        return rsl;
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
+        return tracker.delete(id);
     }
 }
