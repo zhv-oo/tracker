2d961fe1d4e44b20cee77950c7ce57f457c7857d
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-12 07:45:47 +0700

    1. Телефонный справочник на базе ArrayList [#41598 #166811]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -23,7 +23,10 @@ public class PhoneDictionaryTest {
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
+        phones.add(
+                new Person("Ivan", "Ivanovich", "124872", "Rostov")
+        );
         ArrayList<Person> persons = phones.find("487");
-        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+        assertThat(persons.size(), is(2));
     }
 }
