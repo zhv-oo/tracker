72e23622bdfc872008573b0e3b560003307e23c1
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-13 10:20:30 +0700

    2. Очередь с приоритетом на LinkedList [#41670 #166821]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -13,7 +13,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
             for (Task element : tasks) {
-                if (element.getPriority() >= task.getPriority()) {
+                if (element.getPriority() > task.getPriority()) {
                     break;
                 }
                 index++;
