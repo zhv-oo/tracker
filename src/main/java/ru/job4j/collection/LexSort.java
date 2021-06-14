fdda84add0f4453aa9face9fa5d1cbf23c10c62f
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-14 12:40:09 +0700

    4. Сортировка номера [#221284 #167425]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,16 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(getNumber(left), getNumber(right));
-    }
-
-    private int getNumber(String line) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int i = 0;
-        while (line.charAt(i) != '.') {
-            stringBuilder.append(line.charAt(i));
-            i++;
-        }
-        return Integer.parseInt(stringBuilder.toString());
+        String[] srcOne = left.split("\\. ");
+        String[] srcTwo = right.split("\\. ");
+        return Integer.compare(Integer.parseInt(srcOne[0]), Integer.parseInt(srcTwo[0]));
     }
 }
