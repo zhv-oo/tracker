cceb388f63c5e7f82ce5d6c8d4304434ad557641
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-15 18:09:12 +0700

    2. Отсортировать департаменты [#34352 #167806]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,11 +1,17 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return !o1.substring(0, 2).equals(o2.substring(0, 2))
-                ? o2.compareTo(o1) : o1.compareTo(o2);
+        String[] strOne = o1.split("/");
+        String[] strTwo = o2.split("/");
+        int rsl = strOne[0].compareTo(strTwo[0]);
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
+        } else {
+            rsl = o2.compareTo(o1);
+        }
+        return rsl;
     }
 }
