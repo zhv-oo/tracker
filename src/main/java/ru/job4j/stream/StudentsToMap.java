0c28ab2826f71f4467da7d062620685be6d8dc82
zhv-oo <zhitenev.oleg@yandex.ru>
2021-06-22 10:35:59 +0700

    4. Преобразование List в Map. [#110226]

---
+++ src/main/java/ru/job4j/stream/StudentsToMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StudentsToMap {
+    public Map<String, Student> listToMap(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(
+                        key -> key.getSurname(),
+                        value -> value
+                ));
+    }
+}
---
+++ src/test/java/ru/job4j/stream/StudentsToMapTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class StudentsToMapTest {
+    @Test
+    public void items4To3Maps() {
+        StudentsToMap stm = new StudentsToMap();
+        List<Student> students = List.of(
+                new Student(4, "Ivanov"),
+                new Student(4, "Petrov"),
+                new Student(3, "Krasnov"),
+                new Student(5, "Sidorov")
+        );
+        Map<String, Student> result = stm.listToMap(students);
+        assertEquals(students.get(2), result.get("Krasnov"));
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void whenDuplicateKey() {
+        StudentsToMap stm = new StudentsToMap();
+        List<Student> students = List.of(
+                new Student(4, "Ivanov"),
+                new Student(4, "Petrov"),
+                new Student(3, "Ivanov"),
+                new Student(5, "Sidorov")
+        );
+        Map<String, Student> result = stm.listToMap(students);
+    }
+}
