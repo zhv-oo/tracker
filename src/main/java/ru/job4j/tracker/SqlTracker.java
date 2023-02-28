ad7c015ab15604846d91d788a3fddabc21cb3404
zhv-oo <zhitenev.oleg@yandex.ru>
2023-02-28 16:05:55 +0300

    1. Liquibase. Установка. [#504892]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -17,7 +17,7 @@ public class SqlTracker implements Store {
 
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader()
-                .getResourceAsStream("app.properties")) {
+                .getResourceAsStream("./db/liquibase.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
